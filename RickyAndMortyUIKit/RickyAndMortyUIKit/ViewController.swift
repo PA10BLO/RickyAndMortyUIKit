56ff5b0ca31bd692cd2a4fa130af4f84cae5dc45
Pablo Ureña <pablourena10@gmail.com>
2025-09-20 13:00:09 +0200

    initial project

---
+++ RickyAndMortyUIKit/RickyAndMortyUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyAndMortyUIKit
+//
+//  Created by Pablo on 20/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
